d40c0f6c498bc47ebc37573c4e63e8386e3c442c
alfredlaggner <alfred.laggner@gmail.com>
2020-07-29 10:40:36 -0700

    added customer imports

---
+++ app/Imports/CustomersImport.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Imports;
+
+use App\CustomerImport;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class CustomersImport implements ToModel, WithHeadingRow
+{
+    public function model(array $row)
+    {
+        $phone = '';
+        if (strlen(trim($row['phone'])) == 10) {
+            $phone = "(" . substr($row['phone'], 0, 3) . ") " . substr($row['phone'], 3, 3) . "-" . substr($row['phone'], 6);
+        }
+
+        return new CustomerImport([
+            'license' => $row['license'],
+            'ext_id' => $row['id'],
+            'api_id' => $row['api_id'],
+            'reference_id' => $row['reference_id'],
+            'name' => $row['name'],
+            'business_name' => $row['business_name'],
+            'street' => $row['street'],
+            'street2' => $row['street2'],
+            'city' => $row['city'],
+            'zip' => $row['zip'],
+            'territory' => $row['territory'],
+            'email' => $row['email'],
+            'phone' => $phone,
+            'license_type' => $row['license_type'],
+            'user_id' => $row['user_id'],
+            'expiration' => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['expiration']),
+        ]);
+
+    }
+
+}
